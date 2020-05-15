97f38b774901241d76f1198de3ace529da913da0
Pablolo <pablo_olondriz@hotmail.com>
2020-05-15 17:24:08 +0200

    get driver route slightly modified

--- routes/driver.js
+++ routes/driver.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const Driver = require('../models/User');
+const Car = require('../models/Car');
 
 const router = express.Router();
 
 // GET /driver/:id page
 router.get('/:id', async (req, res, next) => {
   try {
-    const driver = await Driver.findById(req.params.id);
-    return res.status(200).json(driver);
+    // const driver = await Driver.findById(req.params.id);
+    const publishedCars = await Car.find({ owner: req.params.id });
+    return res.status(200).json(publishedCars);
   } catch (error) {
     next(error);
   }
