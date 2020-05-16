9f29028218e7bfbb450ec77dc25c081efef239ea
Pablolo <pablo_olondriz@hotmail.com>
2020-05-16 19:53:56 +0200

    modify delete router

--- routes/driver.js
+++ routes/driver.js
@@ -3,6 +3,8 @@ const express = require('express');
 const Driver = require('../models/User');
 const Car = require('../models/Car');
 
+const { destructureCars, destructureOneCar } = require('../helpers/createAndUpdateCar');
+
 const router = express.Router();
 
 // GET /driver/:id page
@@ -10,7 +12,13 @@ router.get('/:id', async (req, res, next) => {
   try {
     const user = await Driver.findById(req.params.id);
     const publishedCars = await Car.find({ owner: req.params.id });
-    return res.status(200).json({ publishedCars, user });
+    if (publishedCars.length === 1) {
+      const carDestructured = destructureOneCar(publishedCars);
+      res.status(200).json({ carDestructured, user });
+    } else {
+      const carDestructured = destructureCars(publishedCars);
+      res.status(200).json({ carDestructured, user });
+    }
   } catch (error) {
     next(error);
   }
