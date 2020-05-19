f0d47fb57f3836e663a705d27576cf6dc43ee6ef
Pablolo <pablo_olondriz@hotmail.com>
2020-05-19 19:37:34 +0200

    whoami destructured response for user data

--- routes/driver.js
+++ routes/driver.js
@@ -12,7 +12,6 @@ router.get('/:id', async (req, res, next) => {
   try {
     const user = await Driver.findById(req.params.id);
     const publishedCars = await Car.find({ owner: req.params.id });
-    console.log('publishedcaaaaars', publishedCars)
     if (publishedCars.length === 0) {
       res.status(200).json({ user });
     } else if (publishedCars.length === 1) {
@@ -22,14 +21,6 @@ router.get('/:id', async (req, res, next) => {
       const carDestructured = destructureCars(publishedCars);
       res.status(200).json({ carDestructured, user });
     }
-
-    // if (publishedCars.length === 1) {
-    //   const carDestructured = destructureOneCar(publishedCars);
-    //   res.status(200).json({ carDestructured, user });
-    // } else {
-    //   const carDestructured = destructureCars(publishedCars);
-    //   res.status(200).json({ carDestructured, user });
-    // }
   } catch (error) {
     next(error);
   }
