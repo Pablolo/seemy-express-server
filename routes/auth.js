9a3852f0d65b841cca4b0910f888e3646496e00d
Pablolo <pablo_olondriz@hotmail.com>
2020-05-21 20:57:37 +0200

    login validation done

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const { check, validationResult } = require('express-validator');
-const { checkEmailAndPasswordNotEmpty } = require('../middlewares');
 
 const User = require('../models/User');
 
@@ -53,22 +52,36 @@ router.post(
   }
 );
 
-router.post('/login', checkEmailAndPasswordNotEmpty, async (req, res, next) => {
-  const { email, password } = res.locals.auth;
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ code: 'not found' });
+router.post(
+  '/login',
+  [
+    check('email')
+      .isEmail()
+      .normalizeEmail(),
+    // check('password').isLength({ min: 8 }),
+  ],
+  async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(422).json({ errors: errors.array() });
+      }
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ code: 'Invalid Email or Password' });
+      }
+      if (bcrypt.compareSync(password, user.hashedPassword)) {
+        req.session.currentUser = user;
+        return res.json(user);
+      }
+      return res.status(404).json({ code: 'Invalid Email or Password' });
+    } catch (error) {
+      next(error);
     }
-    if (bcrypt.compareSync(password, user.hashedPassword)) {
-      req.session.currentUser = user;
-      return res.json(user);
-    }
-    return res.status(404).json({ code: 'not-found' });
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 router.get('/logout', (req, res, next) => {
   req.session.destroy(err => {
